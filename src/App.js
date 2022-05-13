2ac0d9108fcf7c1e3aba0433d5826e77700469a0
Royce <roycecopley@hotmail.com>
2022-05-13 14:01:50 +0100

    First edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
+
+// import BarChart from './BarChart';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+// import Create from "./Create";
+// import BlogDetails from "./BlogDetails";
+// import CalcApp from "./CalcApp";
+// import FigmaSample from "./FigmaSample";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          OG text in place
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            {/* <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="/calculator">
+              <CalcApp />
+            </Route>
+            <Route path="/figma">
+              <FigmaSample />
+            </Route>
+            <Route path="/barchart">
+              <BarChart />
+            </Route> */}
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+// import BlogList from "./BlogList";
+// import useFetch from "./useFetch";
+
+const Home = () => {
+    let name = 'Constant'; // not reactive
+    const [name2, setName2] = useState('Variable'); // this change is reactive
+    const [age, setAge] = useState(38); // this is also reactive
+
+    const handleClick = () => {
+        name = 'Changed Constant';
+        setName2('Changed Variable');
+        setAge(44);
+    }
+
+    // const { data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+
+    //const [name3, setName3] = useState('Mario');
+
+    /*const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }*/
+
+    
+    return ( 
+        <div className="home">
+            {/* {error && <div>{ error}</div>} */}
+            <h2>This is the homepage! v2</h2>
+            {/*<p>{ name2 } is {age} years old</p>*/}
+            <p>{ name }</p>
+            <p>{ name2 }</p>
+            <button onClick={handleClick}>Update Variable</button>
+            {/* {isPending && <div>loading...</div>} */}
+            {/* {blogs && <BlogList blogs={blogs} title='All Blogs!' />} */}
+            {/*{blogs && <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />}
+            {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="Mario's Blogs" />}
+            <button onClick={() => setName3('Luigi')}>change name</button>
+            <p>{ name3 }</p>*/}
+        </div>
+     );
+}
+ 
+export default Home;
