614d31756107b69bdee8c500eeb157898b90898e
Royce <roycecopley@hotmail.com>
2022-05-16 13:30:18 +0100

    upload build

---
+++ build/precache-manifest.d5aa39290e8fc667856bcf6c529e92ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3155795d4b8eadfc6b11",
+    "url": "/static/css/main.94b2e114.chunk.css"
+  },
+  {
+    "revision": "3155795d4b8eadfc6b11",
+    "url": "/static/js/main.3155795d.chunk.js"
+  },
+  {
+    "revision": "e7e0e04a5ea3842eb42e",
+    "url": "/static/js/1.e7e0e04a.chunk.js"
+  },
+  {
+    "revision": "7dd90910e444b842f90b",
+    "url": "/static/js/2.7dd90910.chunk.js"
+  },
+  {
+    "revision": "6a4751f849122296e7c6",
+    "url": "/static/js/runtime~main.6a4751f8.js"
+  },
+  {
+    "revision": "5950f0a66484adb019a0871cae628a83",
+    "url": "/index.html"
+  }
+];
