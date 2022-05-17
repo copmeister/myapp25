e354bce5068d553c0f9d70fe91f0704fc0ab1307
Royce <roycecopley@hotmail.com>
2022-05-17 09:01:59 +0100

    Repair

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>App</h1>
+            <h1>Test App</h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 {/* <Link to="/create">New Page</Link>
